ff0b9e9567b50254eccf3bf772a3eec47504d092
ByrDen <comeit@outlook.com>
2025-05-12 05:52:04 +0300

    replace psycopg onto asyncpg, fix architecture

---
+++ app/config.py
@@ -0,0 +1,51 @@
+from pathlib import Path
+
+from pydantic import SecretStr, PositiveInt, Field
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from sqlalchemy import MetaData
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+
+
+class Config(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        # extra="ignore",
+        case_sensitive=False,
+    )
+
+    BASE_DIR: Path = Path(__file__).resolve().parent.parent
+
+    pguser: str = "admin"
+    postgres_user: str = Field(default="admin", alias="POSTGRES_USER")
+    postgres_password: str = Field(default="admin", alias="POSTGRES_PASSWORD")
+    postgres_host: str = Field(default="127.0.0.1:5432", alias="POSTGRES_HOST")
+    postgres_db: str = Field(default="admin", alias="POSTGRES_DB")
+    postgres_echo: bool = True
+
+    @property
+    def postgres_url(self) -> str:
+        return (f"postgresql+asyncpg://"
+                f"{self.postgres_user}"
+                f":{self.postgres_password}"
+                f"@{self.postgres_host}"
+                f"/{self.postgres_db}")
+
+
+    JWT_ACCESS_SECRET_KEY: SecretStr = ""
+    JWT_REFRESH_SECRET_KEY: SecretStr = ""
+    JWT_ACCESS_ALGORITHM: str = "HS256"
+    JWT_REFRESH_ALGORITHM: str = "HS512"
+    JWT_ACCESS_EXP: PositiveInt = 15
+    JWT_REFRESH_EXP: PositiveInt = 60
+    JWT_TOKEN_TYPE: str = "Bearer"
+
+
+config = Config()
+
+engine = create_async_engine(
+    url=config.postgres_url,
+    echo=config.postgres_echo,
+)
+db_meta = MetaData()
+sessionmaker = async_sessionmaker(bind=engine, expire_on_commit=False)
+
---
+++ app/setup_db.py
@@ -0,0 +1,17 @@
+import asyncio
+import faulthandler
+
+# import app.__main__
+from app.config import engine
+from app.models import Base
+
+
+async def reinit_db() -> None:
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
+        await conn.run_sync(Base.metadata.create_all)
+
+
+if __name__ == '__main__':
+    faulthandler.enable()
+    asyncio.run(reinit_db())
