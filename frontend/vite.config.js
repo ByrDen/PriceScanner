eaf0b3be330ea72cec3f2a2489fdb0ca77957f6f
ByrDen <comeit@outlook.com>
2025-05-14 02:19:53 +0300

    feat: empty BarcodeScanner with docker

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: ['5eadcdb2f09e55.lhr.life'],
+  },
+    build: {
+    outDir: 'dist', // должно быть dist
+  },
 })
+
