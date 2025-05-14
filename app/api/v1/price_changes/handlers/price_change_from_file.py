d6c3adcd84ba127ef92f465bff3f1af5c094073d
ByrDen <comeit@outlook.com>
2025-05-15 00:53:03 +0300

    feat: ...

--- app/api/v1/price_changes/handlers/price_change_from_file.py
+++ app/api/v1/price_changes/handlers/price_change_from_file.py
@@ -3,7 +3,7 @@ from starlette import status
 
 from app.models import PriceChange
 from app.models.base_db import db
-# from ..dependencies import clean_price_change_table
+from ..dependencies import clean_price_change_table
 from ..utils import get_changes_price_data_from_excel_file
 
 
@@ -14,13 +14,13 @@ router = APIRouter()
 @router.post(path="/products/change/file/upload",status_code=201)
 async def upload_file_excel(
     file: UploadFile,
-    # clean_table: bool = False,
+    clean_table: bool = False,
     ) -> dict[str, str]:
     if file.filename.split(sep=".")[1] not in ["xlsx", "xls"]:
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Incorrect type of file")
 
-    # if clean_table:
-    #     await clean_price_change_table()
+    if clean_table:
+        await clean_price_change_table()
 
     input_data = await get_changes_price_data_from_excel_file(file=file)
 
